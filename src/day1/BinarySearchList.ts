c43c7406e1d79e74bef85b9c2708b94b420de557
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-06-06 09:50:17 +0800

    Binary Search Algo

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,13 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[mid];
+        if (v == needle) return true;
+        else if (v < needle) lo = mid + 1;
+        else hi = mid;
+    } while (lo < hi);
 
+    return false;
 }
