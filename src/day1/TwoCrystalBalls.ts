15b25c250d40216c252f9c2c7b559b3620fdf969
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-06-06 17:22:09 +0800

    bubble sort and 2 crystall ball algo

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+//Given 2 crystal balls that will break if dropped from a high enough distance, determine the exact spot in which they’ll break in the most optimized way.
 
+// [false, false, false, true, true ]
+
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmt = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmt;
+    for (; i < breaks.length; i += jumpAmt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    for (let j = i - jumpAmt; j < i; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
 }
