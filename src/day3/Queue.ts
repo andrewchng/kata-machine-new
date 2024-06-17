e145c2a7cbbe840d8c877f8be1825625b25a0769
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-06-17 11:15:33 +0800

    practiced q and stack

--- src/day3/Queue.ts
+++ src/day3/Queue.ts
@@ -1,18 +1,45 @@
+type node<T> = {
+    prev?: node<T>;
+    value: T;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    public head: node<T> | undefined;
+    public tail: node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        // join q from tail
+        const node = { value: item } as node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.head = this.tail = node;
+        }
 
-}
+        this.tail.prev = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+        // exit q from head
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const exitNode = this.head;
+        this.head = this.head.prev;
 
-}
+        if (this.length == 0) {
+            this.tail = this.head = undefined;
+        }
+
+        return exitNode.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day3/Stack.ts
+++ src/day3/Stack.ts
@@ -1,18 +1,40 @@
+type node<T> = {
+    prev?: node<T>;
+    value: T;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    public head: node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        this.length++;
+        const node = { value: item } as node<T>;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
 
-}
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
 
-}
+        const pop = this.head;
+        this.head = pop.prev;
+
+        return pop.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
