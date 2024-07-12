6d355b05399ab2ec4114379206dffc4c3ce74582
andrewchng92 <andrewchng92@gmail.com>
2024-07-12 14:33:59 +0800

    practiced qs

--- src/day6/QuickSort.ts
+++ src/day6/QuickSort.ts
@@ -1,3 +1,33 @@
-export default function quick_sort(arr: number[]): void {
+function partition(arr: number[], lo: number, hi: number) : number{
+    let pivot = hi;
+    let idx = lo - 1;
 
+    for ( let i = lo; i < hi; i++){
+        if ( arr[i] < arr[pivot]){
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+
+    idx++;
+    [arr[idx], arr[pivot]] = [arr[pivot], arr[idx]];
+
+    return idx
+}
+
+function qs(arr: number[], lo:number, hi:number){
+    if ( lo >= hi){
+        return
+    }
+
+    const pivot = partition(arr, lo, hi)
+
+    qs(arr, lo, pivot-1)
+    qs(arr, pivot+1, hi)
+
+
+}
+
+export default function quick_sort(arr: number[]): void {
+ qs(arr, 0, arr.length-1)
 }
