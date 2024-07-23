08364132c17330ba908f0742cfdac918e45c9995
andrewchng92 <andrewchng92@gmail.com>
2024-07-23 11:43:37 +0800

    practiced binarySearch + quicksort

--- src/day8/BinarySearchList.ts
+++ src/day8/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length - 1;
+    
+    while(lo <= hi){
+        let  mid = Math.floor(lo + (hi - lo) / 2);
+        if (haystack[mid] == needle){
+            return true;
+        }
 
+        if (needle > haystack[mid]){
+            lo = mid + 1 
+        }else{
+            hi = mid
+        }
+
+    }
+
+
+    return false;
 }
+ 
--- src/day9/QuickSort.ts
+++ src/day9/QuickSort.ts
@@ -1,3 +1,31 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number) {
+    if (hi <= lo) {
+        return;
+    }
 
+    let pivot = partition(arr, lo, hi);
+
+    qs(arr, lo, pivot - 1);
+    qs(arr, pivot + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] < pivot) {
+            idx++;
+            [arr[idx], arr[i]] = [arr[i], arr[idx]];
+        }
+    }
+
+    idx++;
+    [arr[idx], arr[hi]] = [arr[hi], arr[idx]];
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
