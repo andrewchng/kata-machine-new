5bfc9687b6f35941ef82715daa1c8b11539aa871
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-08-29 14:50:03 +0800

    traverse order:

--- src/day12/BTPostOrder.ts
+++ src/day12/BTPostOrder.ts
@@ -1,3 +1,15 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = []
+    walk(head, path);
+    return path
+}
 
+function walk(cur: BinaryNode<number> | null, path: number[]) {
+    if (!cur) {
+        return;
+    }
+
+    walk(cur.left, path);
+    walk(cur.right, path);
+    path.push(cur.value);
 }
--- src/day12/BTPreOrder.ts
+++ src/day12/BTPreOrder.ts
@@ -1,3 +1,15 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = []
+    walk(head, path);
+    return path
+}
 
+function walk(cur: BinaryNode<number> | null, path: number[]) {
+    if (!cur) {
+        return;
+    }
+
+    path.push(cur.value);
+    walk(cur.left, path);
+    walk(cur.right, path);
 }
