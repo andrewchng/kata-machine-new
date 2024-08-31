08e8be55a62442f70c5e1ca4e020e5d900aefc46
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-08-31 11:38:23 +0800

    DFS on graphlist

--- src/day12/DFSGraphList.ts
+++ src/day12/DFSGraphList.ts
@@ -1,3 +1,31 @@
 export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const path : number[] = [];
+    const seen : boolean[] = new Array(graph.length).fill(false);
 
+    walk(graph, source, needle, seen, path);
+
+    return path.length? path : null;
+}
+
+function walk(graph: WeightedAdjacencyList, cur : number, needle: number, seen : boolean[], path : number[]): boolean{
+    if(cur === needle){
+        path.push(cur);
+        return true;
+    }
+
+    if(seen[cur]){
+        return false;
+    }
+
+    path.push(cur);
+    seen[cur] = true;
+
+    for(let graphEdge of graph[cur]){
+        if(walk(graph, graphEdge.to, needle, seen, path)){
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;   
 }
