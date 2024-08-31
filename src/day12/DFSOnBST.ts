a13e4603c9f340e39ab660852c9e71ccb15f5101
Andrew Chng Wei Lun <andrewchng92@gmail.com>
2024-08-31 11:43:32 +0800

    DFS on BST

--- src/day12/DFSOnBST.ts
+++ src/day12/DFSOnBST.ts
@@ -1,3 +1,22 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+ return walk(head, needle);
+}
 
+function walk(cur : BinaryNode<number> | null, needle: number) :boolean{
+    if(!cur){
+        return false;
+    }
+    if(cur.value === needle){
+        return true;
+    }
+
+    if(walk(cur.left, needle)){
+        return true
+    }
+
+    if(walk(cur.right, needle)){
+        return true;
+    }
+
+    return false;
 }
