b615a38e1a89ffcba97a5b97dd55b0daf5fdbb65
andrewchng92 <andrewchng92@gmail.com>
2024-09-21 16:07:07 +0800

    practiced queue/stack

--- src/day12/Queue.ts
+++ src/day12/Queue.ts
@@ -1,18 +1,47 @@
+type node<T> = {
+    value: T;
+    next: node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    public head: node<T> | undefined;
+    public tail: node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as node<T>;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            this.length++;
+            return;
+        }
+        this.length++;
+        this.tail.next = node;
+        this.tail = node;
+    }
 
-}
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        const value = this.head.value;
+        if (this.length == 1) {
+            this.length--;
+            this.head = this.tail = undefined;
+            return value;
+        }
+        this.head = this.head.next;
+        this.length--;
+        return value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
--- src/day12/Stack.ts
+++ src/day12/Stack.ts
@@ -1,18 +1,50 @@
+type node<T> = {
+    value: T;
+    prev?: node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head: node<T> | undefined;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node = { value : item } as node<T>;
+        if(!this.head){
+            this.head = node;
+            this.length++;
+            return;
+        }
 
-}
+        this.length++;
+        node.prev = this.head;
+        this.head = node;
+    }
+
+
     pop(): T | undefined {
+        if(!this.head){
+            return undefined;
+        }
 
-}
+        const value = this.head.value;
+        if(this.length == 1){
+            this.head = undefined;
+            this.length--;
+            return value
+        }
+
+        this.length--;
+        this.head = this.head.prev
+        return value;
+    }
+
+
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
