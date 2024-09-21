ca1b91a37ef4686209300541c646fd79058558f2
andrewchng92 <andrewchng92@gmail.com>
2024-09-21 16:49:34 +0800

    practiced minheap

--- src/day12/MinHeap.ts
+++ src/day12/MinHeap.ts
@@ -1,15 +1,86 @@
 export default class MinHeap {
     public length: number;
-
-    
+    public data: number[];
 
     constructor() {
+        this.length = 0;
+        this.data = [];
     }
 
     insert(value: number): void {
+        this.data.push(value);
+        this.heapifyUp(this.length);
+        this.length++;
+    }
 
-}
     delete(): number {
+        const toDelete = this.data[0];
+        if (this.length == 1) {
+            this.data = [];
+            this.length--;
+            return toDelete;
+        }
+        this.data[0] = this.data[this.length - 1];
+        this.data.pop();
+        this.length--;
+        this.heapifyDown(0);
 
-}
+        return toDelete;
+    }
+
+    parent(id: number): number {
+        return Math.floor((id - 1) / 2);
+    }
+
+    rChild(id: number): number {
+        return 2 * id + 2;
+    }
+    lChild(id: number): number {
+        return 2 * id + 1;
+    }
+
+    heapifyUp(id: number) {
+        if (id === 0) {
+            return;
+        }
+
+        const parentId = this.parent(id);
+        const parentVal = this.data[parentId];
+        const currVal = this.data[id];
+        if (currVal < parentVal) {
+            [this.data[id], this.data[parentId]] = [
+                this.data[parentId],
+                this.data[id],
+            ];
+            this.heapifyUp(parentId);
+        }
+    }
+
+    heapifyDown(id: number) {
+        const rightId = this.rChild(id);
+        const leftId = this.lChild(id);
+        const rightVal = this.data[rightId];
+        const leftVal = this.data[leftId];
+        const currVal = this.data[id];
+
+        if (!leftVal || !currVal) {
+            return;
+        }
+
+        if (leftVal < rightVal && leftVal < currVal) {
+            [this.data[leftId], this.data[id]] = [
+                this.data[id],
+                this.data[leftId],
+            ];
+            this.heapifyDown(leftId);
+        }
+
+        if (rightVal < leftVal && rightVal < currVal) {
+            [this.data[rightId], this.data[id]] = [
+                this.data[id],
+                this.data[rightId],
+            ];
+            this.heapifyDown(rightId);
+        }
+    }
 }
--- src/day12/QuickSort.ts
+++ src/day12/QuickSort.ts
@@ -1,3 +1,31 @@
 export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
 
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo > hi) {
+        return;
+    }
+
+    let pivot = partition(arr, lo, hi);
+
+    qs(arr, lo, pivot - 1);
+    qs(arr, pivot + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    let pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            [arr[idx], arr[i]] = [arr[i], arr[idx]];
+        }
+    }
+
+    idx++;
+    [arr[idx], arr[hi]] = [arr[hi], arr[idx]];
+
+    return idx;
 }
