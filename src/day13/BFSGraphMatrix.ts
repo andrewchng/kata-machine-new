90c8c4b345a1e465cb48e79a68c2a33ce23d7b56
andrewchng92 <andrewchng92@gmail.com>
2024-09-22 15:26:55 +0800

    practiced BFS on Graph and Dijkstra

--- src/day13/BFSGraphMatrix.ts
+++ src/day13/BFSGraphMatrix.ts
@@ -1,3 +1,45 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const queue: number[] = [source];
+    seen[source] = true;
 
+    while (queue.length > 0) {
+        const cur = queue.shift() as number;
+        const adj = graph[cur];
+        if(cur === needle){
+            break;
+        }
+        for (let i = 0; i < adj.length; i++) {
+            if(seen[i]){
+                continue;
+            }
+            if (adj[i] === 0) {
+                continue;
+            }
+            seen[i] = true;
+            prev[i] = cur;
+            queue.push(i);
+        }
+    }
+
+    if(prev[needle] === -1){
+        return null;
+    }
+
+    let cur = needle;
+    let out = [];
+    while(prev[cur] != -1){
+        out.push(cur);
+        cur = prev[cur];
+    }
+    out.push(source);
+
+    return out.reverse();
 }
+
+//
--- src/day13/DijkstraList.ts
+++ src/day13/DijkstraList.ts
@@ -1,3 +1,61 @@
-export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
+export default function dijkstra_list(
+    source: number,
+    sink: number,
+    arr: WeightedAdjacencyList,
+): number[] {
+    const seen: boolean[] = new Array(arr.length).fill(false);
+    const prev: number[] = new Array(arr.length).fill(-1);
+    const dists: number[] = new Array(arr.length).fill(Infinity);
+    dists[source] = 0;
 
+    while (hasUnvisited(dists, seen)) {
+        const cur = getLowestUnvisited(dists, seen);
+        seen[cur] = true;
+
+        const adj: GraphEdge[] = arr[cur];
+        for (let i = 0; i < adj.length; i++) {
+            const edge = adj[i];
+            if (seen[edge.to]) {
+                continue;
+            }
+            const newDist = dists[cur] + edge.weight;
+            if (newDist < dists[edge.to]) {
+                dists[edge.to] = newDist;
+                prev[edge.to] = cur;
+            }
+        }
+    }
+
+    if (prev[sink] == -1) {
+        return [];
+    }
+
+    let cur = sink;
+    let out = [];
+    while (prev[cur] !== -1) {
+        out.push(cur);
+        cur = prev[cur];
+    }
+
+    out.push(source);
+
+    return out.reverse();
+}
+
+function hasUnvisited(dists: number[], seen: boolean[]) {
+    return seen.some((s, i) => !s && dists[i] < Infinity);
+}
+
+function getLowestUnvisited(dists: number[], seen: boolean[]): number {
+    let idx = -1;
+    let lowest = Infinity;
+
+    for (let i = 0; i < seen.length; i++) {
+        if (seen[i]) continue;
+        if (lowest > dists[i]) {
+            lowest = dists[i];
+            idx = i;
+        }
+    }
+    return idx;
 }
