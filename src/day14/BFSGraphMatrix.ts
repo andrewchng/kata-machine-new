af37329200cd44b3b494714de71f6b69aa1142f8
andrewchng92 <andrewchng92@gmail.com>
2024-10-19 17:49:19 +0800

    practiced minheap + bfsGraphMatrix

--- src/day14/BFSGraphMatrix.ts
+++ src/day14/BFSGraphMatrix.ts
@@ -1,3 +1,35 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const prev: number[] = new Array(graph.length).fill(-1);
+    const seen: boolean[] = new Array(graph.length).fill(false);
 
+    const queue: number[] = [source];
+    while (queue.length) {
+        const cur = queue.shift() as number;
+        if (cur === needle) break;
+        const adj = graph[cur];
+        for (let i = 0; i < adj.length; i++) {
+            if (seen[i]) continue;
+            if (adj[i] === 0) continue;
+            prev[i] = cur;
+            seen[i] = true;
+            queue.push(i);
+        }
+    }
+
+    if (prev[needle] === -1) return null;
+
+    let cur = needle;
+    let out = [];
+    while (prev[cur] !== -1) {
+        out.push(cur);
+        cur = prev[cur];
+    }
+
+    out.push(source);
+
+    return out.reverse();
 }
--- src/day14/MinHeap.ts
+++ src/day14/MinHeap.ts
@@ -1,15 +1,82 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.length = 0;
+        this.data = [];
     }
 
     insert(value: number): void {
-
-}
+        this.data.push(value);
+        this.length++;
+        this.heapifyUp(this.length - 1);
+    }
     delete(): number {
+        const deleteValue = this.data[0];
+        [this.data[0], this.data[this.length-1]] = [this.data[this.length-1], this.data[0]];
+        this.data.pop();
+        this.length--;
+        this.heapifyDown(0);
 
-}
+        return deleteValue;
+    }
+
+    parentId(id: number) {
+        return Math.floor((id - 1 )/ 2);
+    }
+
+    rChildId(id: number) {
+        return 2 * id + 2;
+    }
+
+    lChildId(id: number) {
+        return 2 * id + 1;
+    }
+
+    heapifyUp(id: number) {
+        if (id === 0) {
+            return;
+        }
+
+        const parentId = this.parentId(id);
+        const parentVal = this.data[parentId];
+        const currVal = this.data[id];
+        if (parentVal > currVal) {
+            [this.data[parentId], this.data[id]] = [
+                this.data[id],
+                this.data[parentId],
+            ];
+            this.heapifyUp(parentId);
+        }
+    }
+
+    heapifyDown(id: number) {
+        const rChildId = this.rChildId(id);
+        const lChildId = this.lChildId(id);
+
+        const curr = this.data[id];
+        const lChildData = this.data[lChildId];
+        const rChildData = this.data[rChildId];
+
+        if (!curr || !lChildData) {
+            return;
+        }
+
+        if (lChildData < rChildData && curr > lChildData) {
+            [this.data[lChildId], this.data[id]] = [
+                this.data[id],
+                this.data[lChildId],
+            ];
+            this.heapifyDown(lChildId);
+        }
+
+        if (rChildData < lChildData && curr > rChildData) {
+            [this.data[rChildId], this.data[id]] = [
+                this.data[id],
+                this.data[rChildId],
+            ];
+            this.heapifyDown(rChildId);
+        }
+    }
 }
