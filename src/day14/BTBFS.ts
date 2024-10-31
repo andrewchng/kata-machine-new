d996f248286fc8f23fbd94688d15bfafeed8a38e
andrewchng92 <andrewchng92@gmail.com>
2024-10-31 22:13:25 +0800

    practiced BTBFS

--- src/day14/BTBFS.ts
+++ src/day14/BTBFS.ts
@@ -1,3 +1,17 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let queue = [head];
 
+    while(queue.length){
+        const cur = queue.shift() as BinaryNode<number>;
+        if (cur.value === needle) return true;
+
+        if(needle < cur.value){
+            if(cur.left) queue.push(cur.left);
+        }else{
+            if(cur.right) queue.push(cur.right);
+        }
+
+    }
+
+    return false;
 }
