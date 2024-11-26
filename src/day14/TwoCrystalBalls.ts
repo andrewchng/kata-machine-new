c24d729180d47685058fb264a7527127ac10d933
andrewchng92 <andrewchng92@gmail.com>
2024-11-26 15:20:54 +0800

    practiced

--- src/day14/TwoCrystalBalls.ts
+++ src/day14/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const step = Math.floor(Math.sqrt(breaks.length));
 
+    let breakPoint = 0;
+    for(let i = 0; i < breaks.length; i += step){
+        if(breaks[i]){
+            breakPoint = i;
+            break;
+        }
+    }
+
+    for(let i = breakPoint - step; i < breakPoint; i++){
+        if(breaks[i]){
+            return i;
+        }
+
+    }
+    return -1;
 }
