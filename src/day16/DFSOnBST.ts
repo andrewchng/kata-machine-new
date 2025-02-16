18bdb33223c3fe13549b75ac0d151c6af5c5dc92
andrewchng92 <andrewchng92@gmail.com>
2025-02-16 21:45:20 +0800

    practiced DFS

--- src/day16/DFSOnBST.ts
+++ src/day16/DFSOnBST.ts
@@ -1,3 +1,17 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return walk(head, needle);
+}
 
+function walk(node: BinaryNode<number> | null, needle: number): boolean {
+    if (node === null) {
+        return false;
+    }
+
+    if (node.value === needle) {
+        return true;
+    }
+
+    return node.value > needle
+        ? walk(node.left, needle)
+        : walk(node.right, needle);
 }
