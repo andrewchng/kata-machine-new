457d92832dd4cfeccf1e2dab61629cdcc22f5d3a
Andrew <andrewchng92@gmail.com>
2025-02-24 21:45:00 +0800

    practiced dfs on BST on neovim

--- src/day17/DFSOnBST.ts
+++ src/day17/DFSOnBST.ts
@@ -1,3 +1,15 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
+}
 
+function search(cur: BinaryNode<number> | null, needle: number): boolean {
+    if (!cur) {
+        return false;
+    }
+
+    if (cur.value === needle) {
+        return true;
+    }
+
+    return needle > cur.value ? search(cur.right, needle) : search(cur.left, needle);
 }
