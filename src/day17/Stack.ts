ee70591708b3a2dde746fb2fb379f17206359212
Andrew <andrewchng92@gmail.com>
2025-02-24 21:37:47 +0800

    practiced stack using neovim

--- src/day17/Stack.ts
+++ src/day17/Stack.ts
@@ -1,18 +1,44 @@
+type node<V> = {
+    value: V,
+    prev: node<V> | undefined
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head: node<T> | undefined;
+    private tail: node<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        const node = { value: item } as node<T>;
+        if (this.length === 0) {
+            this.head = this.tail = node;
+            this.length++;
+            return;
+        }
+        this.length++;
+        node.prev = this.tail;
+        this.tail = node;
+    }
     pop(): T | undefined {
+        if (this.length === 0) return undefined;
+        if (this.length === 1) {
+            this.length--;
+            const popVal = this.head?.value
+            this.head = this.tail = undefined
+            return popVal;
+        }
+        this.length--;
+        const popVal = this.tail?.value;
+        this.tail = this.tail?.prev;
+        return popVal;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
