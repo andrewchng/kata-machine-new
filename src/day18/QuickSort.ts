81a4d7838ac700a24eb01dd1823baba186f137c4
andrewchng <andrewchng92@gmail.com>
2025-03-13 15:57:04 +0800

    practiced qs

--- src/day18/QuickSort.ts
+++ src/day18/QuickSort.ts
@@ -1,3 +1,31 @@
 export default function quick_sort(arr: number[]): void {
-
+    qs(arr, 0, arr.length - 1);
 }
+
+function qs(arr: number[], lo: number, hi: number) {
+    if (lo > hi) {
+        return;
+    }
+
+    let pivot = partition(arr, lo, hi);
+    qs(arr, pivot + 1, hi);
+    qs(arr, lo, pivot - 1);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    let pivot = hi;
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= arr[pivot]) {
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+
+    idx++;
+    [arr[idx], arr[pivot]] = [arr[pivot], arr[idx]];
+
+    return idx;
+}
+
