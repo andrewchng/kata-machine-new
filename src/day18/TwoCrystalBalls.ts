d53f06db5a07b9c52d32dac9823652209cd3313a
Andrew Chng <andrewchng@t410>
2025-05-13 20:14:58 +0800

    updates

--- src/day18/TwoCrystalBalls.ts
+++ src/day18/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpstep = Math.floor(Math.sqrt(breaks.length));
+    let i = 0;
+    for (; i < breaks.length; i += jumpstep) {
+        if (breaks[i]) {
+            break;
+        }
+    }
 
+    let j = i - jumpstep;
+    for (; j < breaks.length; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
