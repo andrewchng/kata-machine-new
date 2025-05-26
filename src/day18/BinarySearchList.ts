fe11376d98b59301b2e37e483f4c38980a58010b
andrewchng-x220 <andrewchng92@gmail.com>
2025-05-26 19:08:32 +0800

    practice bfs

--- src/day18/BinarySearchList.ts
+++ src/day18/BinarySearchList.ts
@@ -1,17 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
-    let hi = haystack.length;
-
+    let hi = haystack.length - 1;
     while (lo <= hi) {
-        let mid = Math.floor(lo + (hi - lo) / 2);
-        if (haystack[mid] === needle) {
-            return true;
-        } else if (haystack[mid] < needle) {
+        let mid = Math.floor(lo + (hi - lo));
+        if (haystack[mid] === needle) return true;
+        else if (haystack[mid] < needle) {
             lo = mid + 1;
         } else {
             hi = mid - 1;
         }
     }
-
     return false;
 }
